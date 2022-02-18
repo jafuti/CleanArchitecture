e85d11deedcff498413bdb5fe4aa92f4f02fd820
jafare2010@gmail.com <jafare2010@gmail.com>
2022-02-18 22:07:11 +0300

    Commit Service and Repository Interfaces, Course View Models

---
+++ CleanArchitecture/CleanArch.CoreApplication/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.CoreApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.CoreApplication.Interfaces
+{
+  public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}

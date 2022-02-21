19cebd8e73c24b15cfb888f2923f6cdefe6539b9
jafare2010@gmail.com <jafare2010@gmail.com>
2022-02-21 17:04:34 +0300

    Commit changes for Presention Layer, styling and Authorization

---
+++ CleanArchitecture/CleanArch.MVc/Controllers/CourseController1.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.CoreApplication.Interfaces;
+using CleanArch.CoreApplication.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.MVc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}

d4a77d3510a4a49583097a999e1b623a3acbf55d
jafare2010@gmail.com <jafare2010@gmail.com>
2022-02-26 11:58:32 +0300

    Adding Domain.Core and Implementation of InMemoryBus

--- CleanArchitecture/Clean.Arch.Infra.Bus/InMemoryBus.cs
+++ CleanArchitecture/Clean.Arch.Infra.Bus/InMemoryBus.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Clean.Arch.Domain.Core.Bus;
+using Clean.Arch.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Clean.Arch.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus :IMediatorHandler
     {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        //The bus orchostrates the messages that are going back and forth
+
+        //push the messages through the bus and then you have various connected handlers that are looking for certain types of messages.
+        //in order to send the commands through the bus, it is a kind of wrapper around the mediator that send a method
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
     }
 }
